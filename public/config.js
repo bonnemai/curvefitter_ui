7ced58b0fc0cec8eb4ab58ea9389d03473dd65ea
Olivier Bonnemaison <bonnemai@yahoo.fr>
2025-10-07 09:46:24 +0100

    Use the right URL

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.__APP_CONFIG__ = {
-  STREAM_URL: "https://2pzmybbrkdhzf75k6wz24dflua0hbiko.lambda-url.eu-west-2.on.aws/curves/stream",
+  STREAM_URL: "https://2pzmybbrkdhzf75k6wz24dflua0hbiko.lambda-url.eu-west-2.on.aws/curves",
   APP_ENV: "dev"
 };
--- run.sh
+++ run.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
-set -euo pipefail
+# set -euo pipefail
 
-MODE="${1:-docker}"
-ZIP_NAME="${ZIP_NAME:-curve-fitter-ui-lambda.zip}"
-STREAM_URL='https://2pzmybbrkdhzf75k6wz24dflua0hbiko.lambda-url.eu-west-2.on.aws/curves/stream'  
-if [[ "${MODE}" == "lambda" ]]; then
-  npm run build:lambda
-  pushd lambda/build >/dev/null
-  rm -f "../${ZIP_NAME}"
-  if ! command -v zip >/dev/null 2>&1; then
-    echo "zip utility not found. Install it or archive lambda/build manually." >&2
-    exit 1
-  fi
-  zip -qr "../${ZIP_NAME}" .
-  popd >/dev/null
-  echo "Lambda package created at lambda/${ZIP_NAME}"
-  exit 0
-fi
+# MODE="${1:-docker}"
+# ZIP_NAME="${ZIP_NAME:-curve-fitter-ui-lambda.zip}"
+# STREAM_URL='https://2pzmybbrkdhzf75k6wz24dflua0hbiko.lambda-url.eu-west-2.on.aws/curves/stream'  
+# if [[ "${MODE}" == "lambda" ]]; then
+#   npm run build:lambda
+#   pushd lambda/build >/dev/null
+#   rm -f "../${ZIP_NAME}"
+#   if ! command -v zip >/dev/null 2>&1; then
+#     echo "zip utility not found. Install it or archive lambda/build manually." >&2
+#     exit 1
+#   fi
+#   zip -qr "../${ZIP_NAME}" .
+#   popd >/dev/null
+#   echo "Lambda package created at lambda/${ZIP_NAME}"
+#   exit 0
+# fi
 
 IMAGE_NAME="curve-fitter-ui:latest"
+STREAM_URL=https://2pzmybbrkdhzf75k6wz24dflua0hbiko.lambda-url.eu-west-2.on.aws
 
-docker build -t "${IMAGE_NAME}" .
+docker build -t --build-arg STREAM_URL=$STREAM_URL "${IMAGE_NAME}" .
 
 docker run --rm \
   -p 8080:80 \
